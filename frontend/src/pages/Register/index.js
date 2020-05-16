3c5c6e5b6cc8af3d16dac3c5d979c73a48eaa146
David Moreira <davidmoreira37@gmail.com>
2020-05-16 18:48:57 +0100

    Fix typo in frontend web

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -35,7 +35,7 @@ export default function Register() {
                 <section>
                     <img src={logoImg} alt="be the hero" />
                     <h1>register</h1>
-                    <p>registe your organization</p>
+                    <p>register your organization</p>
                     <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color="#E02041" />
                         login
